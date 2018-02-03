422bac270193608ac4d504a1ce7fda1b4affc598
0l3shka <q0l3shka@gmail.com>
2018-02-03 08:59:25 +0700

    Testing MiniVan class

--- BasicInheritance/Program.cs
+++ BasicInheritance/Program.cs
@@ -15,8 +15,12 @@ namespace BasicInheritance
             Car myCar = new Car(80);
             myCar.Speed = 50;
             Console.WriteLine("My car is going {0} MPH", myCar.Speed);
-            Console.ReadLine();
+           
 
+            MiniVan myVan = new MiniVan();
+            myVan.Speed = 10;
+            Console.WriteLine("My van is going {0} MPH", myVan.Speed);
+            Console.ReadLine();
         }
     }
 }
