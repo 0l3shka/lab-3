fbb6b08ce800f27d32e6305c8233b38598432e45
0l3shka <q0l3shka@gmail.com>
2018-02-03 09:13:36 +0700

    New project, import Employee class from lab-2

---
+++ Employees/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    partial class Employee
+    {
+        //variables
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+        private string empSSN;
+
+   
+
+        //methods
+
+        public void GiveBonus(float amount)
+        {
+            Pay += amount;
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("ID:{0}", ID);
+            Console.WriteLine("Age:{0}", Age);
+            Console.WriteLine("Pay: {0}", Pay);
+
+        }
+
+
+    }
+}
